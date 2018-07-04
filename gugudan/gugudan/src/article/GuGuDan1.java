53053d19f4df1071728909b6abf3e805254a874f
hyeonwoo jo <hyeonwoo1129@naver.com>
2018-07-04 16:40:15 +0900

    gugudan

---
+++ gugudan/gugudan/src/article/GuGuDan1.java
@@ -0,0 +1,16 @@
+package article;
+
+public class GuGuDan1 {
+
+	    public static void main(String[] args) {
+	        for(int i=1; i<=9; i++) {
+	            for(int j=2; j<=9; j++) {
+	                System.out.printf("%d * %d = %2d", j, i, (j*i));
+	                System.out.print("\t");
+	            }
+	            System.out.println();
+	        }
+	    }
+	 
+	
+}
